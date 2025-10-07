ab377a59bfe19febc5d70b3559a5326c90efef9a
Karan gupta <karangupta7481@gmail.com>
2025-10-07 14:43:39 +0530

    database successfully connected

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config(
     {
         path:"./env" // config ek object leta hai jo batata hai ki env variables konse path me hai like yaaha root ke andar .env me h
     }
 )
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running on port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((error)=>{
+    console.log("Mongo DB connection FAiled !!!",error);
+
+});
