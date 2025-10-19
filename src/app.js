3cad4577c2165a77607c6be63adb08ff146a4ccf
Karan gupta <karangupta7481@gmail.com>
2025-10-19 12:11:43 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
 import express from "express"
+import cookieParser from "cookie-parser";
+import cors from "cors"
 const app=express();
-export {app};
+app.use(cors({
+    origin :process.env.ORIGIN,
+    credentials :true
+}))
+app.use(express.json({limit :"12kb"}))  // for json files
+app.use(express.urlencoded({extended :true,
+    limit :"12kb"
+})) // for accepting url type of data and extended allows nesting in objects
+app.use(express.static("public")) // helps to diaplay stattic files like images etc which is as mentioned here in public folder
+app.use(cookieParser())      // cookie parser help krta hai user cookis mai crud applications perform krne mai
+export default app;
