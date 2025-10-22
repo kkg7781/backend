f2d99cfc9c8f24ec0876bff3f98a40baa309f70a
Karan gupta <karangupta7481@gmail.com>
2025-10-22 13:45:06 +0530

    fixed few errors  added notes

--- src/controllers/users.models.js
+++ src/controllers/users.models.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 import bcrypt from "bcrypt"
-import jwt from "jsonwebtokenk"
+import jwt from "jsonwebtoken"
 const userSchema=  new mongoose.Schema(
     {
 username:{
@@ -35,13 +35,13 @@ username:{
 },
 watchHistory:[
     {
-        type:mongoose.Schema.ObjectId,
+        type:mongoose.Schema.Types.ObjectId,
         ref: "Video",
     }
 ],
 password:{
     type:String,
-    required:[true, "Paswoord is required"]
+    required:[true, "Pasword is required"]
 },
 refreshToken:{
     type:String
@@ -58,7 +58,7 @@ userSchema.pre("save", async function (next){    //pre ek middleware hai jo kisi
 ////.isModified("password") returns true only if password is newly set or changed.
 //If it’s not modified, we skip hashing and move on using next().
 
-    this.password=bcrypt.hash(this.password, 10);
+    this.password=await bcrypt.hash(this.password, 10);
     next();
 
 }) /// async → allows us to use await inside (needed because bcrypt.hash() is asynchronous).
@@ -71,7 +71,7 @@ userSchema.methods.generateAccessToken=function(){
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullName: this.fullName
+            fullname: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
@@ -92,3 +92,20 @@ userSchema.methods.generateRefreshToken = function(){
     )
 }
 export const User=mongoose.model("User", userSchema) 
+/* additional notes
+bcrypt :- library for hashing password. it uses an adaptive hashing algo 
+jwt:- used to create and verify json web tokens  for stateless authorisation 
+Optionally stores the refresh token string for session management.
+Storing refresh tokens server-side enables revocation; storing them in DB is common.
+.pre(save) middleware runs before user.save even before user.create
+make sure to use normal function syntax when passing it as callback function so that we can use this keyword 
+this.isModified(password ) returns true if password field has been changed or newly set. 
+if pasword is not updated eg:- full name is updated so we dont rehash password. avoids hashing of already hashed functions
+bcrypt.hash(this.password, 10)  10 is the salt round the more the value the slower it becomes but security increases 
+jwt.sign helps to create a jwt token signed wih ACCESS_TOKEN_SECRET
+payloads are passed here and they are thenb further converted to jwt tokens
+Refresh tokens usually live longer — days to months (e.g., "7d", "30d").
+
+Server can store refresh tokens to enable revocation (storing in DB).
+
+ */ 
